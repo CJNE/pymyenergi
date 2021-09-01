374b933d5a0316cb2485a77010fbade2f0e9150b
Johan Isacsson <johan@generatorhallen.se>
2021-09-01 10:20:12 +0200

    Re-check ASN on connection errors

--- pymyenergi/connection.py
+++ pymyenergi/connection.py
@@ -30,6 +30,7 @@ class Connection:
         self.password = password
         self.auth = httpx.DigestAuth(self.username, self.password)
         self.headers = {"User-Agent": "Wget/1.14 (linux-gnu)"}
+        self.do_query_asn = True
         _LOGGER.debug("New connection created")
 
     def _checkMyenergiServerURL(self, responseHeader):
@@ -50,7 +51,7 @@ class Connection:
         async with httpx.AsyncClient(
             auth=self.auth, headers=self.headers, timeout=self.timeout
         ) as httpclient:
-            if self.base_url is None:
+            if self.base_url is None or self.do_query_asn:
                 _LOGGER.debug("Get Myenergi base url from director")
                 try:
                     directorUrl = self.director_url + "/cgi-jstatus-E"
@@ -59,12 +60,15 @@ class Connection:
                     _LOGGER.error("Myenergi server request problem")
                     _LOGGER.debug(sys.exc_info()[0])
                 else:
+                    self.do_query_asn = False
                     self._checkMyenergiServerURL(response.headers)
             theUrl = self.base_url + url
             try:
                 _LOGGER.debug(f"{method} {url} {theUrl}")
                 response = await httpclient.request(method, theUrl, json=json)
             except httpx.ReadTimeout:
+                # Make sure to query for ASN next request, might be a server problem
+                self.do_query_asn = True
                 raise TimeoutException()
             else:
                 _LOGGER.debug(f"GET status {response.status_code}")
@@ -73,6 +77,8 @@ class Connection:
                     return response.json()
                 elif response.status_code == 401:
                     raise WrongCredentials()
+                # Make sure to query for ASN next request, might be a server problem
+                self.do_query_asn = True
                 raise MyenergiException(response.status_code)
 
     async def get(self, url):
