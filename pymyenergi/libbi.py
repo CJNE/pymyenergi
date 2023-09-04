002057fc4535ecf984c7a1cb047e7d2d0837c92b
Richard Burford <richard@videowalrus.com>
2023-09-04 09:14:42 +0000

    state of charge and all CT

--- pymyenergi/libbi.py
+++ pymyenergi/libbi.py
@@ -29,6 +29,31 @@ class Libbi(BaseDevice):
     def ct3(self):
         """Current transformer 3"""
         return self._create_ct(3)
+    
+    @property
+    def ct4(self):
+        """Current transformer 4"""
+        return self._create_ct(4)
+    
+    @property
+    def ct5(self):
+        """Current transformer 4"""
+        return self._create_ct(5)
+    
+    @property
+    def ct6(self):
+        """Current transformer 4"""
+        return self._create_ct(6)
+    
+    @property
+    def state_of_charge(self):
+        """State of Charge in %"""
+        return self._data.get("soc", 0)
+    
+    @property
+    def test_value(self):
+        """Test value"""
+        return self._data.get("ectp5", 0)
 
     @property
     def prefix(self):
@@ -45,7 +70,13 @@ class Libbi(BaseDevice):
         if short_format:
             return ret
         ret = ret.center(80, "-") + "\n"
+        ret = ret + f"State of Charge: {self.state_of_charge}%\n"
         ret = ret + f"CT 1 {self.ct1.name} {self.ct1.power}W phase {self.ct1.phase}\n"
         ret = ret + f"CT 2 {self.ct2.name} {self.ct2.power}W phase {self.ct2.phase}\n"
         ret = ret + f"CT 3 {self.ct3.name} {self.ct3.power}W phase {self.ct3.phase}\n"
+        ret = ret + f"CT 4 {self.ct4.name} {self.ct4.power}W phase {self.ct4.phase}\n"
+        ret = ret + f"CT 5 {self.ct5.name} {self.ct5.power}W phase {self.ct5.phase}\n"
+        ret = ret + f"CT 6 {self.ct6.name} {self.ct6.power}W phase {self.ct6.phase}\n"
+        ret = ret + f"\nTest Value {self.test_value}\n"
+
         return ret
